8fa707a324be4e2ab8105e975b1c74e677ad9477
ShantnuS <shantnu@outlook.com>
2018-03-10 15:56:14 +0000

    controller

---
+++ Server/src/controller/Controller.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class Controller {
+
+	
+	private Controller() {
+		
+	}
+	
+	private static Controller instance;
+	
+	public static Controller getInstance() {
+		if(instance == null) {
+			instance = new Controller();
+		}
+		return instance;
+	}
+	
+}
