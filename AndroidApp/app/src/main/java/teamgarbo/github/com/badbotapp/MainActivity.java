1e403db9a9e60a2f7af914ebe144337dcc0809b6
ShantnuS <shantnu@outlook.com>
2018-03-10 22:12:48 +0000

    fixed socket code

--- AndroidApp/app/src/main/java/teamgarbo/github/com/badbotapp/MainActivity.java
+++ AndroidApp/app/src/main/java/teamgarbo/github/com/badbotapp/MainActivity.java
@@ -1,7 +1,7 @@
 package teamgarbo.github.com.badbotapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import java.io.IOException;
@@ -14,22 +14,27 @@ import teamgarbo.github.com.badbotapp.message.BadMessage;
 public class MainActivity extends AppCompatActivity {
 
     String ID; //This can be anything that identifies the user
+    Socket socket;
+    ObjectOutputStream os;
+    boolean socketInitalised = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void initSocket() throws IOException {
+        String ip = "10.9.133.81";
+        InetAddress adr =  InetAddress.getByName(ip);
+
+        socket = new Socket(adr, 4444);
+        os = new ObjectOutputStream(socket.getOutputStream());
+        socketInitalised = true;
     }
 
 
     public void sendTestMessage() throws IOException {
-        String ip = "10.9.133.81";
-        InetAddress adr =  InetAddress.getByName(ip);
-
-        Socket socket = new Socket(adr, 4444);
-        ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-
         // Send first message
         BadMessage test = new BadMessage("i", "l");
         os.writeObject(test);
@@ -41,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         {
             public void run() {
                 try {
+                    if(!socketInitalised) initSocket();
                     sendTestMessage();
                 } catch (IOException e) {
                     e.printStackTrace();
