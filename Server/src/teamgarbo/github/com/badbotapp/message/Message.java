524b9de377cf68f553687b675621d72bb027fb2d
smoorc0 <samuel@moorcroft.me.uk>
2018-03-11 08:00:11 +0000

    superclass constructor added

--- Server/src/teamgarbo/github/com/badbotapp/message/Message.java
+++ Server/src/teamgarbo/github/com/badbotapp/message/Message.java
@@ -6,6 +6,11 @@ public abstract class Message implements Serializable{
 
 	String clubID;
 	String playerID;
+
+	public Message(	String clubID, String playerID){
+		this.clubID = clubID;
+		this.playerID = playerID;
+	}
 	
 	public String getClubID() {
 		return clubID;
