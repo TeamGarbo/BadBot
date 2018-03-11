796a4e21d7fdbfa42d103e4f810365b5553b196b
mallocc <gh@alexeverett.uk>
2018-03-11 10:56:41 +0000

    linked new user to main

--- AndroidApp/app/src/main/java/teamgarbo/github/com/badbotapp/NewUserFormActivity.java
+++ AndroidApp/app/src/main/java/teamgarbo/github/com/badbotapp/NewUserFormActivity.java
@@ -1,9 +1,11 @@
 package teamgarbo.github.com.badbotapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class NewUserFormActivity extends AppCompatActivity {
@@ -39,6 +41,23 @@ public class NewUserFormActivity extends AppCompatActivity {
 
     public void submitNewUser(View view)
     {
+        EditText playerNameEditText = findViewById(R.id.player_name);
+        String playerName = playerNameEditText.getText().toString();
 
+        Spinner spinner = findViewById(R.id.color_spinner);
+        String playerBand = (String) spinner.getSelectedItem();
+
+        int elo = 0;
+        for(int i = 0; i < colorBands.length; ++i)
+            if(colorBands[i].equals(playerBand))
+                elo = 5*i;
+
+        Intent data = new Intent();
+        data.putExtra("player_name", playerName);
+        data.putExtra("player_band", elo + "");
+
+        setResult(RESULT_OK,data);
+
+        finish();
     }
 }
